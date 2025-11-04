032d22faa505d88259e7adc272434e2fd2597510
xaliferi <xaliferi@uwo.ca>
2025-11-04 18:22:33 -0500

    fixing routing

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,13 @@
-/** @type {import('next').NextConfig}  */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  images: { unoptimized: true },
-  typescript: { ignoreBuildErrors: true },
-};
+  output: 'export', // Enables static HTML export
+  images: {
+    unoptimized: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  trailingSlash: true, // Optional, avoids routing issues on GH Pages
+}
 
-export default nextConfig;
+export default nextConfig
